d229fed68cd39d11ec986e0dbada55d2e99af84d
Joe Gravelle <jgravelle@ufl.edu>
2021-02-10 21:58:13 -0500

    fixing issue with last commit

--- esac-dashboard-app/src/tests/createName.test.ts
+++ esac-dashboard-app/src/tests/createName.test.ts
@@ -1,4 +1,4 @@
-import { createName } from './createName'
+import { createName } from '../functions/createName'
 
 test('Tests Name Creation', () => {
   expect(createName('Eric', 'Andre')).toEqual('EricAndre')
