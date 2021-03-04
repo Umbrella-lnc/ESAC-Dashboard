d29c1824d78f391ddf0b6ec0be5f605db7b88535
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2021-03-04 10:04:43 -0500

    Cd'd twice into server folder before running

--- server/server.js
+++ server/server.js
@@ -16,11 +16,11 @@ const PORT = process.env.PORT || 5000;
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   //Set static folder
+  app.use(express.static("../client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  });
 }
-app.use(express.static("../client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
-});
 
 // Middleware
 app.use("/posts", postRoutes);
