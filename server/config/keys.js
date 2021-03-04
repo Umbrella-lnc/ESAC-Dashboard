bf48cb043e7f552ac792b2e0840d29ffa96ab9bc
Merrick Ryman <merrick@ctsfla.com>
2021-03-04 12:48:55 -0500

    Refactor index.js -> server.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    ATLAS_URI: "MONGOURI HERE"
+    ATLAS_URI: "mongodb+srv://merrick-ryman:ipfBUzPFvkedkmdn@cluster0.cjvq1.mongodb.net/users?retryWrites=true&w=majority",
     secretOrKey: "secret"
 }
--- server/server.js
+++ server/server.js
@@ -1,26 +1,32 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import postRoutes from './routes/posts.js'
-import dotenv from 'dotenv'
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const passport = require('passport');
+const users = require('./routes/api/users');
 
 // Configure Environment Vars
-dotenv.config();
-const PORT = process.env.PORT || 5001;
-const ATLAS_URI = process.env.ATLAS_URI;
+//dotenv.config();
+const PORT = process.env.PORT || 5000;
+//const ATLAS_URI = process.env.ATLAS_URI;
 
 // Create Express Server
 const app = express();
 
 
 // Middleware
-app.use('/posts', postRoutes);
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
+
+app.use('./routes/api/users', users);
+
 // Database Connection
-mongoose.connect(ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = require("./config/keys").ATLAS_URI;
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const conn = mongoose.connection;
 
@@ -31,6 +37,5 @@ conn.once('open', () => {
 app.listen(PORT, () => {
                 console.log(`Running on port: ${PORT}`);
 })
-
 
 mongoose.set('useFindAndModify', false);
