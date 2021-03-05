311ae419aefa667416dced979b14480e86dad807
Maanasi Vadlamani <maanasivadlamani@gmail.com>
2021-03-05 10:22:57 -0500

    added ESAC message and title to landing page

--- client/src/components/reusable-components/Landing.js
+++ client/src/components/reusable-components/Landing.js
@@ -3,18 +3,23 @@ import { BrowserRouter, Link } from "react-router-dom";
 
 class Landing extends Component {
   render() {
+    const subheadingStyle = {
+      fontFamily: "monospace",
+      fontSize: 15
+    }
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
             <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>ESAC</span> stack from
-              scratch
+              <b>Empowering Engineers!</b>
+              <p></p>
+              <span style={{subheadingStyle}}
+              >Undergraduate Engineering Student Advisory Council</span> 
             </h4>
             <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
+              A bridge between the Engineering 
+              Department and student body.
             </p>
             <br />
             <div className="col s6">
