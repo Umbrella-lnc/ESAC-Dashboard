37b460278610afc05e0b55dc3aebc4fe56bdcb1d
Maanasi Vadlamani <maanasivadlamani@gmail.com>
2021-03-05 09:16:08 -0500

    imported Navbar.js and Landing.js into App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Navbar from "./components/reusable-components/Navbar";
+import Landing from "./components/reusable-components/Landing";
+
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <Navbar />
+        <Landing />
       </div>
     );
   }
