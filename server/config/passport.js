79bdb25fd701ae9367c11c3d0444774a1d559570
Joe Gravelle <jgravelle@ufl.edu>
2021-03-08 19:39:48 -0500

    code from 3rd tutorial - linking redux and react
    There are issues with my local system (no .env file) that I need to fix that are giving me problems. For that reason I can't test that the front end connects with the db yet. But here are the changes I've made.

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,27 +1,29 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const mongoose = require('mongoose');
-const VerifiedUser = mongoose.model("verifiedUsers");
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const mongoose = require('mongoose')
+const VerifiedUser = mongoose.model('verifiedUsers')
 //const UnverifiedUser = mongoose.model("unverifiedUsers");
 
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 
-dotenv.config();
-const secretOrKey = process.env.secretOrKey;
+dotenv.config()
+const secretOrKey = process.env.secretOrKey
 
-const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = secretOrKey;
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+opts.secretOrKey = secretOrKey
 
-module.exports = passport => {
-    passport.use(
-        new JwtStrategy(opts, (jwt_payload, done) => {
-            VerifiedUser.findById(jwt_payload.id).then(user => {
-                if(user) {
-                    return done(null, user);
-                }
-                return done(null, false);
-            }).catch(err => console.log(err));
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      VerifiedUser.findById(jwt_payload.id)
+        .then((user) => {
+          if (user) {
+            return done(null, user)
+          }
+          return done(null, false)
         })
-    );
-};
+        .catch((err) => console.log(err))
+    })
+  )
+}
