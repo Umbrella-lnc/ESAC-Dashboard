d26247eef25a4b9b61b990edcc40e4bee46b3592
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2021-03-10 13:36:36 -0500

    Fixed radio button selection. Updated state with either representative or administrator

--- client/src/components/Access-level-radio-button.js
+++ client/src/components/Access-level-radio-button.js
@@ -1,41 +1,43 @@
-import React from 'react'
-import Radio from '@material-ui/core/Radio'
-import RadioGroup from '@material-ui/core/RadioGroup'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import FormControl from '@material-ui/core/FormControl'
-import FormLabel from '@material-ui/core/FormLabel'
+import React from "react";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
 
 export default function RadioButtonsGroup(props) {
-  const { changeAccess } = props
+  const { changeAccess, getAccess } = props;
 
   return (
-    <FormControl component='fieldset'>
-      <FormLabel component='legend' color='grey'>
+    <FormControl component="fieldset">
+      <FormLabel component="legend" color="grey">
         Access Level
       </FormLabel>
       <RadioGroup
         row
-        color='grey'
-        aria-label='access-level'
-        name='access-level-1'
+        color="grey"
+        aria-label="access-level"
+        name="access-level-1"
       >
         <FormControlLabel
-          value='representative'
-          id='rep'
-          control={<Radio color='primary' />}
-          label='Representative'
-          labelPlacement='end'
-          onClick={() => changeAccess(id)}
+          value="representative"
+          id="rep"
+          control={<Radio color="primary" />}
+          label="Representative"
+          labelPlacement="end"
+          onClick={(e) => changeAccess(e.target.value)}
+          checked={"representative" == getAccess()}
         />
         <FormControlLabel
-          value='administrator'
-          id='admin'
-          control={<Radio color='primary' />}
-          label='Administrator'
-          labelPlacement='end'
-          onClick={() => changeAccess(id)}
+          value="administrator"
+          id="admin"
+          control={<Radio color="primary" />}
+          label="Administrator"
+          labelPlacement="end"
+          onClick={(e) => changeAccess(e.target.value)}
+          checked={"administrator" == getAccess()}
         />
       </RadioGroup>
     </FormControl>
-  )
+  );
 }
