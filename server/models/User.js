6dc151f092e070cb0f89f8d3e027040600ea08fb
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2021-03-11 19:50:55 -0500

    Consolidated users into one collection. Verified users will have active field true.

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    accessLevel: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { collection: "Users" }
+);
+
+module.exports = VerifiedUser = mongoose.model("Users", userSchema);
