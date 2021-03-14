ba66bffdff1956dd07906d9116b4c71838d6982e
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2021-03-14 00:22:43 -0500

    Made template authorized route.

--- server/routes/api/userRouter.js
+++ server/routes/api/userRouter.js
@@ -1,5 +1,6 @@
 const userRoutes = require("../../controllers/userController.js");
 const express = require("express");
+const passport = require("passport");
 
 const router = express.Router();
 
@@ -9,5 +10,14 @@ router.get("/", (req, res) => {
     // access by localhost:5000/api/users/
     res.send("It works!!!");
 });
+
+//Testing authentification, just send a get request with bearer token in auth
+router.get(
+    "/secure",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        res.send("You are authenticated, sending web page to: " + req.user);
+    }
+);
 
 module.exports = router;
