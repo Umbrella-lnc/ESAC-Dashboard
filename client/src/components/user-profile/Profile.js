2007487c2d89e0883c6977e1c01ed8acd2181214
Maanasi Vadlamani <maanasivadlamani@gmail.com>
2021-03-18 14:20:40 -0400

    interpolated user name,email,department,and accessLevel

--- client/src/components/user-profile/Profile.js
+++ client/src/components/user-profile/Profile.js
@@ -18,70 +18,79 @@ class Profile extends Component {
     return (
       <div style={{ height: '75vh' }} className='container valign-wrapper'>
         <div className='row'>
-          <div class="section">
-            <div className='col s12 left-align'>
-               <img src={ProfileIcon} width="450" length="500"></img>
+          <div className="col">
+            <div className='col s6 center-align'>
+               <img src={ProfileIcon} width="400" length="500"></img>
             </div>
-            <h4 className='col s6 offset-s6 left-align'>
-              <b>Profile</b> 
-            </h4>
-            <div className='col s12 offset-s6 left-align'>
-              <h6>
-                <p className='flow-text grey-text text-darken-1'>
-                  Email:
-                </p>
-                <p className='flow-text grey-text text-darken-1'>
-                  Department:
-                </p>
-                <p className='flow-text grey-text text-darken-1'>
-                  Access Level:
-                </p>
-              </h6>
-               <button
+          </div>
+          <div className="col">
+            <div class="section">
+              <h4 className='col s6 offset-s6 left-align'>
+                <b>Profile</b> 
+              </h4>
+              <div className='col s12 offset-s6 left-align'>
+                <h6>
+                  <p className='flow-text grey-text text-darken-1'>
+                    Name: {user.firstname} {user.lastname}
+                  </p>
+                  <p className='flow-text grey-text text-darken-1'>
+                    Email: {user.email}
+                  </p>
+                  <p className='flow-text grey-text text-darken-1'>
+                    Department: {user.department}
+                  </p>
+                  <p className='flow-text grey-text text-darken-1'>
+                    Access Level: {user.accessLevel}
+                  </p>
+                </h6>
+                <button
+                    style={{
+                      width: '170px',
+                      borderRadius: '3px',
+                      letterSpacing: '1.5px',
+                      marginTop: '1rem',
+                    }}
+                    //onClick={}
+                    className='btn btn-large waves-effect waves-light hoverable blue accent-3'>
+                    Edit Profile
+                  </button>
+              </div>
+            </div>
+            <div class="divider"></div>
+            <div class="section">
+            
+            
+              <h4 className='col s6 offset-s6 left-align'>
+                <b>User Management</b> 
+              </h4>
+              <div class="vertical-btn-gp" className='col s6 push-s6'>
+                <button
                   style={{
-                    width: '170px',
+                    width: '400px',
                     borderRadius: '3px',
                     letterSpacing: '1.5px',
                     marginTop: '1rem',
                   }}
                   //onClick={}
-                  className='btn btn-large waves-effect waves-light hoverable blue accent-3'>
-                  Edit Profile
+                  className='btn btn-large waves-effect waves-light hoverable blue accent-3'
+                >
+                  View Representative Profiles
                 </button>
-            </div>
-          </div>
-          <div class="divider"></div>
-          <div class="section">
-            <h4 className='col s6 offset-s6 left-align'>
-              <b>User Management</b> 
-            </h4>
-            <div class="vertical-btn-gp" className='col s6 push-s6'>
-              <button
-                style={{
-                  width: '400px',
-                  borderRadius: '3px',
-                  letterSpacing: '1.5px',
-                  marginTop: '1rem',
-                }}
-                //onClick={}
-                className='btn btn-large waves-effect waves-light hoverable blue accent-3'
-              >
-                View Representative Profiles
-              </button>
-              <button 
-                style={{
-                  clear:"left",
-                  width: '400px',
-                  borderRadius: '3px',
-                  letterSpacing: '1.5px',
-                  marginTop: '1rem',
+                <button 
+                  style={{
+                    clear:"left",
+                    width: '400px',
+                    borderRadius: '3px',
+                    letterSpacing: '1.5px',
+                    marginTop: '1rem',
 
-                }}
-                //onClick={}
-                className='btn btn-large waves-effect waves-light hoverable blue accent-3'
-              >
-                View Administrator Profiles
-              </button>
+                  }}
+                  //onClick={}
+                  className='btn btn-large waves-effect waves-light hoverable blue accent-3'
+                >
+                  View Administrator Profiles
+                </button>
+              </div>
             </div>
           </div> 
         </div>
