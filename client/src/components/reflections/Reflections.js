6517bc80a8663db9fa8aae5e43e3b7702956374a
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2021-03-24 11:10:01 -0400

    Fixed reflections going inside navbar

--- client/src/components/reflections/Reflections.js
+++ client/src/components/reflections/Reflections.js
@@ -3,75 +3,88 @@ import { Grid, Card, CardContent, Typography } from "@material-ui/core";
 import axios from "axios";
 import baseURL from "../../baseURL";
 import jwt_decode from "jwt-decode";
-import PropTypes from "prop-types"
-
+import PropTypes from "prop-types";
 
 class Reflections extends Component {
     state = {
-        reflections: []
-    }
+        reflections: [],
+    };
 
     componentDidMount() {
-        const token = localStorage.getItem('jwtToken')
-        const user = jwt_decode(token)
+        const token = localStorage.getItem("jwtToken");
+        const user = jwt_decode(token);
 
-        if(user.accessLevel === "administrator") {
-            axios.get(baseURL + '/api/reflections/getAllReflections').then((res) => {
-                // Debug
-                console.log(baseURL + '/api/reflections/getAllReflections');
-                this.setState({ reflections: res.data });
-            });
+        if (user.accessLevel === "administrator") {
+            axios
+                .get(baseURL + "/api/reflections/getAllReflections")
+                .then((res) => {
+                    // Debug
+                    console.log(baseURL + "/api/reflections/getAllReflections");
+                    this.setState({ reflections: res.data });
+                });
         } else {
-            axios.get(baseURL + '/api/reflections/getDepartmentReflections').then((res) => {
-                // Debug
-                console.log(baseURL + '/api/reflections/getDepartmentReflections');
-                this.setState({ reflections: res.data });
-            });
+            axios
+                .get(baseURL + "/api/reflections/getDepartmentReflections")
+                .then((res) => {
+                    // Debug
+                    console.log(
+                        baseURL + "/api/reflections/getDepartmentReflections"
+                    );
+                    this.setState({ reflections: res.data });
+                });
         }
     }
 
     render() {
-        if(!this.state.reflections) {
+        if (!this.state.reflections) {
             return null;
         }
 
         return (
-            <div className="container valign-wrapper">
+            <div
+                className="container valign-wrapper"
+                style={{ marginTop: "100px" }}
+            >
                 <Grid
                     container
                     spacing={3}
                     //direction="column"
                     //alignItems="center"
                     //justify="center"
-                >{this.state.reflections.map(reflection => {
-                    return (
-                        <Grid item xs = {12}>
-                            <Card>
-                                <CardContent>
-                                    <Typography variant="h4">
-                                        {reflection.title}
-                                    </Typography>
-                                    <Typography variant="subtitle1"
-                                                color={reflection.status==="Complete" ? 
-                                                    "primary" : "secondary"
-                                    }>
-                                        {reflection.status}
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    )
-                })}
+                >
+                    {this.state.reflections.map((reflection) => {
+                        return (
+                            <Grid item xs={12}>
+                                <Card>
+                                    <CardContent>
+                                        <Typography variant="h4">
+                                            {reflection.title}
+                                        </Typography>
+                                        <Typography
+                                            variant="subtitle1"
+                                            color={
+                                                reflection.status === "Complete"
+                                                    ? "primary"
+                                                    : "secondary"
+                                            }
+                                        >
+                                            {reflection.status}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        );
+                    })}
                 </Grid>
             </div>
-        )
+        );
     }
 }
 
-Reflections.propTypes = {}
+Reflections.propTypes = {};
 
 const mapStateToProps = (state) => ({
-  auth: state.auth,
-})
+    auth: state.auth,
+});
 
 export default Reflections;
