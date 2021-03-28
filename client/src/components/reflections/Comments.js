c5422485c6e70a200a6ab7f7fc75d299b869d542
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2021-03-27 23:47:16 -0400

    Starting comment system.

---
+++ client/src/components/reflections/Comments.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Divider } from "@material-ui/core";
+
+export default function Comments(props) {
+    const { user, id, comments } = props;
+
+    return <div></div>;
+}
