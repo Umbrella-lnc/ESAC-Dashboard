4abcb65b59c4b07c53db71560afee73de61873fb
Joe Gravelle <jgravelle@ufl.edu>
2021-04-12 19:55:40 -0400

    fix to manageProfiles to fix error merrick talked about
    basically a redisgn of the old manageProfiles setup to make the page easier to use

--- client/src/components/reusable-components/ProfileCard.js
+++ client/src/components/reusable-components/ProfileCard.js
@@ -5,7 +5,7 @@ import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import Modal from '@material-ui/core/Modal'
+import CardActionArea from '@material-ui/core/CardActionArea'
 import Dialog from '@material-ui/core/Modal'
 import baseURL from '../../baseURL'
 import axios from 'axios'
@@ -73,20 +73,29 @@ export default function ProfileCard(props) {
     const userContainer = {
         email: email,
     }
-    const [open, setOpen] = React.useState(false)
+    const [confirmOpen, setConfirmOpen] = React.useState(false)
+    const [moreDetailsOpen, setMoreDetailsOpen] = React.useState(false)
 
-    const handleOpen = () => {
-        setOpen(true)
+    const handleConfirmOpen = () => {
+        setConfirmOpen(true)
     }
 
-    const handleClose = () => {
-        setOpen(false)
+    const handleConfirmClose = () => {
+        setConfirmOpen(false)
+    }
+
+    const handleMoreDetailsOpen = () => {
+        setMoreDetailsOpen(true)
+    }
+
+    const handleMoreDetailsClose = () => {
+        setMoreDetailsOpen(false)
     }
 
     function handleDelete() {
         axios.post(baseURL + `/api/usersManagement/deleteUser`, userContainer)
         window.location.reload(true)
-        handleClose()
+        handleConfirmClose()
     }
 
     function handleVerify() {
@@ -97,132 +106,11 @@ export default function ProfileCard(props) {
         window.location.reload(true)
     }
 
-    if (access != 'administrator') {
-        if (active === false) {
-            return (
-                <div>
-                    <Card className={classes.root} raised={true}>
-                        <CardContent className={classes.content}>
-                            <Typography
-                                className={classes.title}
-                                color='textSecondary'
-                                gutterBottom
-                            >
-                                {access}
-                            </Typography>
-                            <Typography variant='h5' component='h2'>
-                                {firstName}
-                            </Typography>
-                            <Typography
-                                className={classes.pos}
-                                color='textSecondary'
-                            >
-                                {email}
-                            </Typography>
-                        </CardContent>
-                        <CardActions className={classes.buttonContainer}>
-                            <List>
-                                <Button
-                                    className={classes.deleteButton}
-                                    size='small'
-                                    onClick={handleOpen}
-                                >
-                                    Delete
-                                </Button>
-                                <Button
-                                    className={classes.activateButton}
-                                    size='small'
-                                    onClick={handleVerify}
-                                >
-                                    Activate
-                                </Button>
-                            </List>
-                        </CardActions>
-                    </Card>
-                    <Dialog open={open} onClose={handleClose}>
-                        <div className={classes.paper}>
-                            <h5 style={{ color: 'black' }}>
-                                Are you sure you would like to delete this user?
-                            </h5>
-                            <Button
-                                onClick={handleDelete}
-                                className={classes.deleteButton}
-                                size='small'
-                            >
-                                yes
-                            </Button>
-                            <Button
-                                onClick={handleClose}
-                                className={classes.deleteButton}
-                                size='small'
-                            >
-                                No
-                            </Button>
-                        </div>
-                    </Dialog>
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <Card className={classes.root} raised={true}>
-                        <CardContent className={classes.content}>
-                            <Typography
-                                className={classes.title}
-                                color='textSecondary'
-                                gutterBottom
-                            >
-                                {access}
-                            </Typography>
-                            <Typography variant='h5' component='h2'>
-                                {firstName}
-                            </Typography>
-                            <Typography
-                                className={classes.pos}
-                                color='textSecondary'
-                            >
-                                {email}
-                            </Typography>
-                        </CardContent>
-                        <CardActions className={classes.buttonContainer}>
-                            <Button
-                                className={classes.deleteButton}
-                                size='small'
-                                onClick={handleOpen}
-                            >
-                                delete
-                            </Button>
-                        </CardActions>
-                    </Card>
-                    <Dialog open={open} onClose={handleClose}>
-                        <div className={classes.paper}>
-                            <h5 style={{ color: 'black' }}>
-                                Are you sure you would like to delete this user?
-                            </h5>
-                            <Button
-                                onClick={handleDelete}
-                                className={classes.deleteButton}
-                                size='small'
-                            >
-                                yes
-                            </Button>
-                            <Button
-                                onClick={handleClose}
-                                className={classes.deleteButton}
-                                size='small'
-                            >
-                                No
-                            </Button>
-                        </div>
-                    </Dialog>
-                </div>
-            )
-        }
-    } else {
-        return (
-            <div>
-                <Card className={classes.root} raised={true}>
-                    <CardContent className={classes.content}>
+    return (
+        <div>
+            <Card className={classes.root} raised={true}>
+                <CardContent className={classes.content}>
+                    <CardActionArea onClick={handleMoreDetailsOpen}>
                         <Typography
                             className={classes.title}
                             color='textSecondary'
@@ -233,15 +121,94 @@ export default function ProfileCard(props) {
                         <Typography variant='h5' component='h2'>
                             {firstName}
                         </Typography>
-                        <Typography
-                            className={classes.pos}
-                            color='textSecondary'
-                        >
-                            {email}
+                        <Typography variant='h5' component='h2'>
+                            {lastName}
                         </Typography>
-                    </CardContent>
-                </Card>
-            </div>
-        )
-    }
+                    </CardActionArea>
+                </CardContent>
+                <CardActions className={classes.buttonContainer}>
+                    <List>
+                        <Button
+                            className={classes.deleteButton}
+                            size='small'
+                            onClick={handleConfirmOpen}
+                        >
+                            Delete
+                        </Button>
+                        {active == false && (
+                            <Button
+                                className={classes.activateButton}
+                                size='small'
+                                onClick={handleVerify}
+                            >
+                                Activate
+                            </Button>
+                        )}
+                    </List>
+                </CardActions>
+            </Card>
+            <Dialog open={confirmOpen} onClose={handleConfirmClose}>
+                <div className={classes.paper}>
+                    <h5 style={{ color: 'black' }}>
+                        Are you sure you would like to delete this user?
+                    </h5>
+                    <Button
+                        onClick={handleDelete}
+                        className={classes.deleteButton}
+                        size='small'
+                    >
+                        yes
+                    </Button>
+                    <Button
+                        onClick={handleConfirmClose}
+                        className={classes.deleteButton}
+                        size='small'
+                    >
+                        No
+                    </Button>
+                </div>
+            </Dialog>
+            <Dialog open={moreDetailsOpen} onClose={handleMoreDetailsClose}>
+                <div className={classes.paper}>
+                    <Typography
+                        className={classes.title}
+                        color='textSecondary'
+                        gutterBottom
+                        style={{ marginTop: '10px' }}
+                    >
+                        {access}
+                    </Typography>
+                    <Typography variant='h5' component='h2'>
+                        {firstName} {lastName}
+                    </Typography>
+                    <Typography className={classes.pos} color='textSecondary'>
+                        {email}
+                    </Typography>
+                    <Button
+                        className={classes.deleteButton}
+                        size='small'
+                        onClick={handleConfirmOpen}
+                    >
+                        Delete
+                    </Button>
+                    {active == false && (
+                        <Button
+                            className={classes.activateButton}
+                            size='small'
+                            onClick={handleVerify}
+                        >
+                            Activate
+                        </Button>
+                    )}
+                    <Button
+                        onClick={handleMoreDetailsClose}
+                        className={classes.deleteButton}
+                        size='small'
+                    >
+                        Close
+                    </Button>
+                </div>
+            </Dialog>
+        </div>
+    )
 }
