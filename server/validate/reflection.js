3643d15637d5d1ee6684c0d1d7588fd2e9727902
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2021-04-15 18:20:39 -0400

    Removed material ui from fontend

--- server/validate/reflection.js
+++ server/validate/reflection.js
@@ -1,6 +1,5 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
-const { ErrorSharp } = require("@material-ui/icons");
 
 module.exports = function validateReflection(data) {
     let errors = {};
@@ -9,7 +8,7 @@ module.exports = function validateReflection(data) {
     data.link = !isEmpty(data.link) ? data.link : "";
     data.department = !isEmpty(data.department) ? data.department : "";
     data.post = !isEmpty(data.post) ? data.post : "";
-    
+
     // Validate department
     const departments = [
         "Mechanical and Aerospace Engineering (MAE)",
@@ -24,11 +23,7 @@ module.exports = function validateReflection(data) {
         "Nuclear Engineering (NE)",
     ];
 
-    const validHosts = [
-        "forms.google.com",
-        "docs.google.com",
-        "forms.gle",
-    ];
+    const validHosts = ["forms.google.com", "docs.google.com", "forms.gle"];
 
     // Validate title
     if (Validator.isEmpty(data.title)) {
@@ -38,17 +33,22 @@ module.exports = function validateReflection(data) {
     // Validate link
     if (Validator.isEmpty(data.link)) {
         errors.link = "Forms link field is required!";
-    } else if(!Validator.isURL(data.link, { protocols: ['https'], require_protocol: true, require_tld: true } )) {
+    } else if (
+        !Validator.isURL(data.link, {
+            protocols: ["https"],
+            require_protocol: true,
+            require_tld: true,
+        })
+    ) {
         errors.link = "Invalid forms link!";
-    } else if(!validHosts.includes(new URL(data.link).host)) {
+    } else if (!validHosts.includes(new URL(data.link).host)) {
         errors.link = "Invalid forms link!";
     }
 
     // Validate department
     if (Validator.isEmpty(data.department)) {
         errors.department = "Department field required!";
-    }
-    else if (!departments.includes(data.department)) {
+    } else if (!departments.includes(data.department)) {
         errors.department = "Invalid department!";
     }
 
