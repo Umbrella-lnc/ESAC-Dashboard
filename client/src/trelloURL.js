65d2633dce47db2b0029eae2ae9305a1f0ad80c3
Joe Gravelle <jgravelle@ufl.edu>
2021-04-16 10:26:38 -0400

    small updates to make merge easier

--- client/src/trelloURL.js
+++
@@ -1,4 +0,0 @@
-export const trelloURL = "https://api.trello.com/1/"
-export const toDoList = "60638369236486515ccc1ec8/";
-export const doingList = "6063836bf49a2c5dc7e08dc6/";
-export const doneList = "6063836cce7e3326413eb0f2/";
