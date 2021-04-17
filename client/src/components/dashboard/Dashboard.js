bb4c6ee72aba4f66b0998c0ee69e3b6d1ac4eed0
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2021-04-16 22:15:54 -0400

    Laying groundwork. Code non-functional.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,139 +1,181 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { List, ListItem } from '@material-ui/core'
-import TrelloCard from '../reusable-components/TrelloCard'
-import ColumnLabel from '../reusable-components/ColumnLabel'
-import baseURL from '../../baseURL'
-import NewCard from '../reusable-components/NewCard'
-import { STATES } from 'mongoose'
+import React, { Component } from "react";
+import axios from "axios";
+import { List, ListItem } from "@material-ui/core";
+import TrelloCard from "../reusable-components/TrelloCard";
+import ColumnLabel from "../reusable-components/ColumnLabel";
+import baseURL from "../../baseURL";
+import NewCard from "../reusable-components/NewCard";
+import { STATES } from "mongoose";
 
 const Dashboard = (props) => {
     const [state, setState] = React.useState({
         lists: [],
         headers: [],
         loading: true,
-    })
+    });
 
     const getList = (listId) => {
         axios
-            .post(baseURL + '/api/trello/getCards', {
+            .post(baseURL + "/api/trello/getCards", {
                 listId: listId,
             })
             .then((res) => {
                 setState((prevState) => ({
                     ...prevState,
                     lists: [...prevState.lists, res.data],
-                }))
+                }));
             })
             .catch(function (error) {
                 if (error.response) {
                     // Request made and server responded
-                    console.log(error.response.data)
-                    console.log(error.response.status)
-                    console.log(error.response.headers)
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
                 } else if (error.request) {
                     // The request was made but no response was received
-                    console.log(error.request)
+                    console.log(error.request);
                 } else {
                     // Something happened in setting up the request that triggered an Error
-                    console.log('Error', error.message)
+                    console.log("Error", error.message);
                 }
-            })
-    }
+            });
+    };
 
     const getCards = () => {
         axios
-            .get(baseURL + '/api/trello/getLists')
+            .get(baseURL + "/api/trello/getLists")
             .then((res) => {
-                console.log(res)
+                console.log(res);
                 setState((prevState) => ({
                     ...prevState,
                     headers: res.data,
-                }))
+                }));
             })
             .catch(function (error) {
                 if (error.response) {
                     // Request made and server responded
-                    console.log(error.response.data)
-                    console.log(error.response.status)
-                    console.log(error.response.headers)
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
                 } else if (error.request) {
                     // The request was made but no response was received
-                    console.log(error.request)
+                    console.log(error.request);
                 } else {
                     // Something happened in setting up the request that triggered an Error
-                    console.log('Error', error.message)
+                    console.log("Error", error.message);
                 }
-            })
-    }
+            });
+    };
 
-    React.useEffect(() => {
-        //Debugging
-        console.log(state)
-    }, [state])
-
-    //Initial Load
-    React.useEffect(() => {
-        getCards()
-    }, [])
-
-    React.useEffect(() => {
-        //If the application has not fetched headers yet
-        if (state.headers.length === 0) {
-            return
+    const getIdFromColumn = (column) => {
+        let idList = "";
+        if (column == "To Do") {
+            idList = "60638369236486515ccc1ec8";
+        } else if (column == "Doing") {
+            idList = "6063836bf49a2c5dc7e08dc6";
+        } else {
+            idList = "6063836cce7e3326413eb0f2";
         }
+        return idList;
+    };
 
-        //Finished loading
-        if (state.lists.length === state.headers.length && state.loading) {
-            setState((prevState) => ({ ...prevState, loading: false }))
+    const editCardColumn = (curColName, index, newColID) => {
+        const oldColIndex = state.headers.findIndex(
+            (header) => header.id === getIdFromColumn(curColName)
+        );
+        const newColIndex = state.headers.findIndex(
+            (header) => header.id === newColID
+        );
+
+        console.log(getIdFromColumn(curColName));
+        console.log(newColID);
+        console.log(curColName + "\n" + index + "\n" + newColID);
+        console.log(oldColIndex + "\n" + index + "\n" + newColIndex);
+        console.log(state.headers);
+
+        //Don't update column if the same
+        if (oldColIndex === newColIndex) {
+            return;
         }
-        //Continue loading
-        else if (state.loading && state.headers.length > 0) {
-            console.log(state.headers)
-            console.log(state.lists.length)
-            getList(state.headers[state.lists.length].id)
-        }
-    }, [state])
+        setState((prevState) => {
+            const newState = { ...prevState };
+            //Add new element to new column
+            newState.lists[newColIndex] = [
+                ...newState.lists[newColIndex],
+                newState.lists[oldColIndex][index],
+            ];
+            //Remove card from old column
+            newState.lists[oldColIndex].splice(index, 1);
+            setState(newState);
+        });
+    };
 
-    const horiList = {
-        display: 'flex',
-        flexDirection: 'row',
-        padding: 0,
-        alignContent: 'flex-start',
-        alignItems: 'flex-start',
-    }
-    const vertiList = {
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 0,
-    }
-    const mainContainer = {
-        padding: 100,
-    }
-    const header = {
-        paddingLeft: '28px',
-    }
-    const subheader = {
-        paddingLeft: '32px',
-    }
-
+    //Hard refresh of cards
     const updateCards = () => {
         setState((prevState) => ({
             ...prevState,
             lists: [],
             loading: true,
-        }))
-    }
+        }));
+    };
 
-    const editCardColumn = (curCol, index, newCol) => {
-        const newIndex = state.headers.indexOf(newCol)
-        setState((prevState) => ({ ...prevState, lists: [...prevState.lists] }))
-    }
+    //POST RENDER
+    React.useEffect(() => {
+        //Debugging
+        console.log(state);
+    }, [state]);
 
+    //Initial Load
+    React.useEffect(() => {
+        getCards();
+    }, []);
+
+    React.useEffect(() => {
+        //If the application has not fetched headers yet
+        if (state.headers.length === 0) {
+            return;
+        }
+
+        //Finished loading
+        if (state.lists.length === state.headers.length && state.loading) {
+            setState((prevState) => ({ ...prevState, loading: false }));
+        }
+        //Continue loading
+        else if (state.loading && state.headers.length > 0) {
+            console.log(state.headers);
+            console.log(state.lists.length);
+            getList(state.headers[state.lists.length].id);
+        }
+    }, [state]);
+
+    //CSS STYLING
+    const horiList = {
+        display: "flex",
+        flexDirection: "row",
+        padding: 0,
+        alignContent: "flex-start",
+        alignItems: "flex-start",
+    };
+    const vertiList = {
+        display: "flex",
+        flexDirection: "column",
+        padding: 0,
+    };
+    const mainContainer = {
+        padding: 100,
+    };
+    const header = {
+        paddingLeft: "28px",
+    };
+    const subheader = {
+        paddingLeft: "32px",
+    };
+
+    //Loading Check
     if (state.loading) {
-        return <h1 style={{ marginTop: '70px' }}>Loading...</h1>
+        return <h1 style={{ marginTop: "70px" }}>Loading...</h1>;
     }
-    //if (state.lists.length > 0) {
+    //render
     return (
         <div style={mainContainer}>
             <h1 style={header}>Tasks</h1>
@@ -144,29 +186,37 @@ const Dashboard = (props) => {
                         <div>
                             <ListItem>
                                 <List style={vertiList}>
-                                    <ListItem className='input-field col s12'>
+                                    <ListItem className="input-field col s12">
                                         <ColumnLabel cardInfo={value.name} />
                                     </ListItem>
 
-                                    {state.lists[index].map((value1) => {
-                                        return (
-                                            <ListItem
-                                                key={value1.id}
-                                                className='input-field col s12'
-                                            >
-                                                <TrelloCard
-                                                    cardInfo={value1}
-                                                    colName={value.name}
-                                                    updateCards={updateCards}
-                                                    editCardColumn={
-                                                        editCardColumn
-                                                    }
-                                                    index={index}
-                                                />
-                                            </ListItem>
-                                        )
-                                    })}
-                                    <ListItem className='input-field col s12'>
+                                    {state.lists[index].map(
+                                        (value1, card_index) => {
+                                            return (
+                                                <ListItem
+                                                    key={value1.id}
+                                                    className="input-field col s12"
+                                                >
+                                                    <TrelloCard
+                                                        cardInfo={value1}
+                                                        colName={value.name}
+                                                        updateCards={
+                                                            updateCards
+                                                        }
+                                                        editCardColumn={
+                                                            editCardColumn
+                                                        }
+                                                        card_index={card_index}
+                                                        col_index={index}
+                                                        getIdFromColumn={
+                                                            getIdFromColumn
+                                                        }
+                                                    />
+                                                </ListItem>
+                                            );
+                                        }
+                                    )}
+                                    <ListItem className="input-field col s12">
                                         <NewCard
                                             colId={value.id}
                                             updateCards={updateCards}
@@ -175,12 +225,11 @@ const Dashboard = (props) => {
                                 </List>
                             </ListItem>
                         </div>
-                    )
+                    );
                 })}
             </List>
         </div>
-    )
-    //   }
-}
+    );
+};
 
-export default Dashboard
+export default Dashboard;
--- client/src/components/reusable-components/TrelloCard.js
+++ client/src/components/reusable-components/TrelloCard.js
@@ -1,47 +1,37 @@
-import React from 'react'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import Typography from '@material-ui/core/Typography'
-import { CardActionArea, List } from '@material-ui/core'
-import axios from 'axios'
-import { useStyles } from './cardStyles'
-import baseURL from '../../baseURL'
-import DeletePopUp from './DeletePopUp'
-import MoreInfoPopUp from './MoreInfoPopUp'
-import EditCardPopUp from './EditCardPopUp'
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { CardActionArea, List } from "@material-ui/core";
+import axios from "axios";
+import { useStyles } from "./cardStyles";
+import baseURL from "../../baseURL";
+import DeletePopUp from "./DeletePopUp";
+import MoreInfoPopUp from "./MoreInfoPopUp";
+import EditCardPopUp from "./EditCardPopUp";
 
 export default function TrelloCard(props) {
-    const classes = useStyles()
+    const classes = useStyles();
 
-    const name = props.cardInfo.name
-    const description = props.cardInfo.desc
-    const colName = props.colName
-    const cardId = props.cardInfo.id
-    const { updateCards } = props
+    const name = props.cardInfo.name;
+    const description = props.cardInfo.desc;
+    const colName = props.colName;
+    const cardId = props.cardInfo.id;
+    const { updateCards, index, editCardColumn, getIdFromColumn } = props;
 
-    const [confirmDeleteOpen, setConfirmDeleteOpen] = React.useState()
-    const [cardDetailsOpen, setCardDetailsOpen] = React.useState(false)
-    const [editCardOpen, setEditCardOpen] = React.useState(false)
+    const [confirmDeleteOpen, setConfirmDeleteOpen] = React.useState();
+    const [cardDetailsOpen, setCardDetailsOpen] = React.useState(false);
+    const [editCardOpen, setEditCardOpen] = React.useState(false);
 
-    var due = String(props.cardInfo.due)
-    if (due.length > 6) {
-        due =
-            due.substring(5, 7) +
-            '/' +
-            due.substring(8, 10) +
-            '/' +
-            due.substring(0, 4)
-    } else {
-        due = ''
-    }
+    var due = String(props.cardInfo.due);
 
-    var label = ''
+    var label = "";
 
     for (var i = 0; i < props.cardInfo.labels.length; i++) {
         if (i != 0) {
-            label = label + ','
+            label = label + ",";
         }
-        label = label + ' ' + props.cardInfo.labels[i].name
+        label = label + " " + props.cardInfo.labels[i].name;
     }
 
     const [state, setState] = React.useState({
@@ -50,222 +40,214 @@ export default function TrelloCard(props) {
         description: description,
         due: due,
         name: name,
-        errors: '',
-    })
+        errors: "",
+    });
 
     const handleConfirmDeleteOpen = () => {
-        setConfirmDeleteOpen(true)
-    }
+        setConfirmDeleteOpen(true);
+    };
 
     const handleConfirmDeleteClose = () => {
-        setConfirmDeleteOpen(false)
-    }
+        setConfirmDeleteOpen(false);
+    };
 
     const handleCardDetailsOpen = () => {
-        setCardDetailsOpen(true)
-    }
+        setCardDetailsOpen(true);
+    };
 
     const handleCardDetailsClose = () => {
-        setCardDetailsOpen(false)
-    }
+        setCardDetailsOpen(false);
+    };
 
     const handleEditCardOpen = () => {
-        setEditCardOpen(true)
-    }
+        setEditCardOpen(true);
+    };
 
     const handleEditCardClose = () => {
         setState({
             ...state,
-            errors: '',
-        })
-        setEditCardOpen(false)
-        setCardDetailsOpen(true)
-    }
+            errors: "",
+        });
+        setEditCardOpen(false);
+        setCardDetailsOpen(true);
+    };
 
     function handleDelete() {
         axios
-            .post(baseURL + '/api/trello/deleteCard', {
+            .post(baseURL + "/api/trello/deleteCard", {
                 cardId: cardId,
             })
             .then(function (res) {
                 //Set cards with setState to avoid window reload
-                updateCards()
+                updateCards();
             })
             .catch(function (error) {
                 if (error.response) {
                     // Request made and server responded
-                    console.log(error.response.data)
-                    console.log(error.response.status)
-                    console.log(error.response.headers)
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
                 } else if (error.request) {
                     // The request was made but no response was received
-                    console.log(error.request)
+                    console.log(error.request);
                 } else {
                     // Something happened in setting up the request that triggered an Error
-                    console.log('Error', error.message)
+                    console.log("Error", error.message);
                 }
-            })
-    }
-
-    const handleColumnChange = (event) => {
-        setState({
-            ...state,
-            column: event.target.value,
-        })
-    }
-
-    const handleDescriptionChange = (event) => {
-        setState({
-            ...state,
-            description: event.target.value,
-        })
-    }
-
-    const handleDueChange = (event) => {
-        setState({
-            ...state,
-            due: event.target.value,
-        })
+            });
     }
 
     const handleOpenEditCardWindow = () => {
-        handleCardDetailsClose()
-        handleEditCardOpen()
-    }
+        handleCardDetailsClose();
+        handleEditCardOpen();
+    };
 
     function isValidDate(dateString) {
         // First check for the pattern
-        if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString)) return false
+        if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString)) return false;
 
         // Parse the date parts to integers
-        var parts = dateString.split('/')
-        var day = parseInt(parts[1], 10)
-        var month = parseInt(parts[0], 10)
-        var year = parseInt(parts[2], 10)
+        var parts = dateString.split("/");
+        var day = parseInt(parts[1], 10);
+        var month = parseInt(parts[0], 10);
+        var year = parseInt(parts[2], 10);
 
         // Check the ranges of month and year
-        if (year < 1000 || year > 3000 || month == 0 || month > 12) return false
+        if (year < 1000 || year > 3000 || month == 0 || month > 12)
+            return false;
 
-        var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
         // Adjust for leap years
         if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-            monthLength[1] = 29
+            monthLength[1] = 29;
 
         // Check the range of the day
-        return day > 0 && day <= monthLength[month - 1]
+        return day > 0 && day <= monthLength[month - 1];
     }
 
-    function handleEditCard() {
-        var idList = ''
+    //Takes in new parameters!
+    function handleEditCard(changes) {
+        const { due, column, description } = changes;
+        const idList = getIdFromColumn(column);
 
-        if ((!isValidDate(state.due) && state.due != '') || state.name == '') {
-            if (!isValidDate(state.due) && state.due != '') {
+        if ((!isValidDate(due) && due != "") || name == "") {
+            if (!isValidDate(due) && due != "") {
                 setState({
                     ...state,
-                    errors: 'invalid date',
-                })
+                    errors: "invalid date",
+                });
             }
-            if (state.name == '') {
+            if (name == "") {
                 setState({
                     ...state,
-                    errors: 'you must include a name',
-                })
+                    errors: "you must include a name",
+                });
             }
-            return
+            return;
         }
 
-        var formattedDue = ''
-        if (state.due != '') {
+        var formattedDue = "";
+        if (due != "") {
             formattedDue =
-                state.due.slice(6, state.due.length) +
-                '-' +
-                state.due.slice(0, 2) +
-                '-' +
-                state.due.slice(3, 5) +
-                'T20:56:00.000Z'
+                due.slice(6, due.length) +
+                "-" +
+                due.slice(0, 2) +
+                "-" +
+                due.slice(3, 5) +
+                "T20:56:00.000Z";
         }
 
-        console.log('state name')
-        console.log(state.column)
-        console.log('colName')
-        console.log(colName)
-        console.log('hello')
-        if (state.column == 'To Do') {
-            console.log('to do')
-            idList = '60638369236486515ccc1ec8'
-        } else if (state.column == 'Doing') {
-            console.log('yep')
-            idList = '6063836bf49a2c5dc7e08dc6'
-        } else {
-            idList = '6063836cce7e3326413eb0f2'
-        }
-        console.log(idList)
+        console.log(idList);
+
         axios
-            .post(baseURL + '/api/trello/editCard', {
+            .post(baseURL + "/api/trello/editCard", {
                 cardId: cardId,
                 idList: idList,
                 due: formattedDue,
-                description: state.description,
+                description: description,
             })
             .then(function (res) {
-                handleEditCardClose()
-                updateCards()
+                handleEditCardClose();
+                //Edit Column
+                editCardColumn(colName, index, idList);
+                //updateCards() Full refresh of cards
+                //Update card info
+                setState((prevState) => ({
+                    ...prevState,
+                    column: column,
+                    description: description,
+                    due: formattedDue,
+                }));
             })
             .catch(function (error) {
                 if (error.response) {
                     // Request made and server responded
-                    console.log(error.response.data)
-                    console.log(error.response.status)
-                    console.log(error.response.headers)
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
                 } else if (error.request) {
                     // The request was made but no response was received
-                    console.log(error.request)
+                    console.log(error.request);
                 } else {
                     // Something happened in setting up the request that triggered an Error
-                    console.log('Error', error.message)
+                    console.log("Error", error.message);
                 }
-            })
+            });
     }
+
+    const readableDate = (due) => {
+        if (due.length > 6) {
+            due =
+                due.substring(5, 7) +
+                "/" +
+                due.substring(8, 10) +
+                "/" +
+                due.substring(0, 4);
+        } else {
+            due = "";
+        }
+        return due;
+    };
 
     return (
         <div>
             <Card className={classes.root} raised={true}>
                 <CardActionArea onClick={handleCardDetailsOpen}>
                     <CardContent className={classes.content}>
-                        {state.due != '' && (
+                        {state.due != "" && (
                             <Typography
                                 className={classes.title}
-                                color='textSecondary'
+                                color="textSecondary"
                                 gutterBottom
                             >
-                                {due}
+                                {readableDate(state.due)}
                             </Typography>
                         )}
-                        {state.due == '' && (
+                        {state.due == "" && (
                             <Typography
                                 className={classes.title}
-                                color='textSecondary'
+                                color="textSecondary"
                                 gutterBottom
                             >
                                 No Due Date
                             </Typography>
                         )}
-                        <Typography variant='h5' component='h2'>
-                            {name}
+                        <Typography variant="h5" component="h2">
+                            {state.name}
                         </Typography>
-                        {state.labels == '' && (
+                        {state.labels == "" && (
                             <Typography
                                 className={classes.pos}
-                                color='textSecondary'
+                                color="textSecondary"
                             >
                                 No Labels
                             </Typography>
                         )}
-                        {state.labels != '' && (
+                        {state.labels != "" && (
                             <Typography
                                 className={classes.pos}
-                                color='textSecondary'
+                                color="textSecondary"
                             >
                                 Labels: {label}
                             </Typography>
@@ -291,18 +273,16 @@ export default function TrelloCard(props) {
             <EditCardPopUp
                 editCardOpen={editCardOpen}
                 handleEditCardClose={handleEditCardClose}
-                name={name}
+                name={state.name}
                 handleConfirmDeleteOpen={handleConfirmDeleteOpen}
                 column={state.column}
-                dueDate={state.due}
+                dueDate={readableDate(state.due)}
                 description={state.description}
-                handleColumnChange={handleColumnChange}
-                handleDueChange={handleDueChange}
                 labels={state.labels}
                 handleEditCard={handleEditCard}
-                handleDescriptionChange={handleDescriptionChange}
                 errors={state.errors}
+                readableDate={readableDate}
             ></EditCardPopUp>
         </div>
-    )
+    );
 }
